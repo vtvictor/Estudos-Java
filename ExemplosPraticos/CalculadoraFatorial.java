feba4cab80663870e2184a96e15e3d944e4146c2
Victor Carvalho <87211894+vtvictor@users.noreply.github.com>
2023-05-09 16:26:28 -0300

    Add files via upload

---
+++ ExemplosPraticos/CalculadoraFatorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculadoraFatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        int fatorial = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println("O fatorial de " + numero + " é " + fatorial);
+
+        scanner.close();
+    }
+} {
+    
+}
