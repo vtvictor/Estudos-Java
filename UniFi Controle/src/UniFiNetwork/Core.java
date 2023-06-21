ae7f4b8e9ef28a4e5865947fcba315fcfd0dd394
vtvictor <victorluiz.bsi@gmail.com>
2023-06-21 10:17:23 -0300

    Repetição para exportação dos dados

--- UniFi Controle/src/UniFiNetwork/Core.java
+++ UniFi Controle/src/UniFiNetwork/Core.java
@@ -32,69 +32,72 @@ public class Core {
 
 			waitFor(2000);
 			
-			//Dados do UniFi do Atendimento:
-			WebElement unifiAtendimento = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[1]/td[3]/div/div/span"));
-			String stringunifiAtendimento = unifiAtendimento.getText();
-			
-			String textoRemovido_atendimento = "AD UNIFI 06 - ";
-			String resultado_atendimento = stringunifiAtendimento.replace(textoRemovido_atendimento, "");
-			
-			WebElement unifiAtendimento_status = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[1]/td[4]/div/div/div/span"));
-			String stringunifiAtendimento_status = unifiAtendimento_status.getText();
-			WebElement unifiAtendimento_experience = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[1]/td[8]/div/div/div/span"));
-			String stringunifiAtendimento_experience = unifiAtendimento_experience.getText();
-
-			
-			//Dados do UniFi do Auditório:
-			WebElement unifiAuditorio = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[2]/td[3]/div/div/span"));
-			String stringunifiAuditorio = unifiAuditorio.getText();
-			
-			String textoRemovido_auditorio = "AD UNIFI 05 - ";
-			String resultado_auditorio = stringunifiAuditorio.replace(textoRemovido_auditorio, "");
-			
-			WebElement unifiAuditorio_status = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[2]/td[4]/div/div/div/span"));
-			String stringunifiAuditorio_status = unifiAuditorio_status.getText();
-			WebElement unifiAuditorio_experience = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[2]/td[8]/div/div/div/span"));
-			String stringunifiAuditorio_experience = unifiAuditorio_experience.getText();
-			
-			
-			//Dados do UniFi da Base SE
-			WebElement unifiBasese = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[3]/td[3]/div/div/span"));
-			String stringunifiBasese = unifiBasese.getText();
-			
-			String textoRemovido_basese = "AD UNIFI 04 - ";
-			String resultado_basese = stringunifiBasese.replace(textoRemovido_basese, "");
-			
-			WebElement unifiBasese_status = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[3]/td[4]/div/div/div/span"));
-			String stringunifiBasese_status = unifiBasese_status.getText();
-			WebElement unifiBasese_experience = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[3]/td[8]/div/div/div/span"));
-			String stringunifiBasese_experience = unifiBasese_experience.getText();
-			
-			
-			
-			//Dados do UniFi Operacional 
-			WebElement unifiOperacional = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[4]/td[3]/div/div/span"));
-			String stringunifiOperacional = unifiOperacional.getText();
-			
-			String textoRemovido_operacional = "AD UNIFI 03 - ";
-			String resultado_operacional = stringunifiOperacional.replace(textoRemovido_operacional, "");
-			
-			WebElement unifiOperacional_status = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[4]/td[4]/div/div/div/span"));
-			String stringunifiOperacional_status = unifiOperacional_status.getText();
-			WebElement unifiOperacional_experience = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[4]/td[8]/div/div/div/span"));
-			String stringunifiOperacional_experience = unifiOperacional_experience.getText();
-			
-			
-			
-			
-			
-			
-			//Exibição de dados para testes:
-			System.out.println("UniFi: " +resultado_atendimento+" | Status: "+stringunifiAtendimento_status+" | Experience: "+stringunifiAtendimento_experience);
-			System.out.println("UniFi: " +resultado_auditorio+" | Status: "+stringunifiAuditorio_status+" | Experience: "+stringunifiAuditorio_experience);
-			System.out.println("Unifi: " +resultado_basese+ " |Status: "+stringunifiBasese_status+" | Experience: "+stringunifiBasese_experience);
+			while (true) {
+				//Dados do UniFi do Atendimento:
+				WebElement unifiAtendimento = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[1]/td[3]/div/div/span"));
+				String stringunifiAtendimento = unifiAtendimento.getText();
+				
+				String textoRemovido_atendimento = "AD UNIFI 06 - ";
+				String resultado_atendimento = stringunifiAtendimento.replace(textoRemovido_atendimento, "");
+				
+				WebElement unifiAtendimento_status = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[1]/td[4]/div/div/div/span"));
+				String stringunifiAtendimento_status = unifiAtendimento_status.getText();
+				WebElement unifiAtendimento_experience = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[1]/td[8]/div/div/div/span"));
+				String stringunifiAtendimento_experience = unifiAtendimento_experience.getText();
+	
+				
+				//Dados do UniFi do Auditório:
+				WebElement unifiAuditorio = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[2]/td[3]/div/div/span"));
+				String stringunifiAuditorio = unifiAuditorio.getText();
+				
+				String textoRemovido_auditorio = "AD UNIFI 05 - ";
+				String resultado_auditorio = stringunifiAuditorio.replace(textoRemovido_auditorio, "");
+				
+				WebElement unifiAuditorio_status = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[2]/td[4]/div/div/div/span"));
+				String stringunifiAuditorio_status = unifiAuditorio_status.getText();
+				WebElement unifiAuditorio_experience = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[2]/td[8]/div/div/div/span"));
+				String stringunifiAuditorio_experience = unifiAuditorio_experience.getText();
+				
+				
+				//Dados do UniFi da Base SE
+				WebElement unifiBasese = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[3]/td[3]/div/div/span"));
+				String stringunifiBasese = unifiBasese.getText();
+				
+				String textoRemovido_basese = "AD UNIFI 04 - ";
+				String resultado_basese = stringunifiBasese.replace(textoRemovido_basese, "");
+				
+				WebElement unifiBasese_status = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[3]/td[4]/div/div/div/span"));
+				String stringunifiBasese_status = unifiBasese_status.getText();
+				WebElement unifiBasese_experience = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[3]/td[8]/div/div/div/span"));
+				String stringunifiBasese_experience = unifiBasese_experience.getText();
+				
+				
+				
+				//Dados do UniFi Operacional 
+				WebElement unifiOperacional = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[4]/td[3]/div/div/span"));
+				String stringunifiOperacional = unifiOperacional.getText();
+				
+				String textoRemovido_operacional = "AD UNIFI 03 - ";
+				String resultado_operacional = stringunifiOperacional.replace(textoRemovido_operacional, "");
+				
+				WebElement unifiOperacional_status = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[4]/td[4]/div/div/div/span"));
+				String stringunifiOperacional_status = unifiOperacional_status.getText();
+				WebElement unifiOperacional_experience = driver.findElement(By.xpath("//*[@id=\"app-main-panel\"]/div[1]/div/div[2]/div/div/table/tbody/tr[4]/td[8]/div/div/div/span"));
+				String stringunifiOperacional_experience = unifiOperacional_experience.getText();
+				
+				
+				
+				
+				
+				
+				//Exibição de dados para testes:
+				System.out.println("UniFi: " +resultado_atendimento+" | Status: "+stringunifiAtendimento_status+" | Experience: "+stringunifiAtendimento_experience);
+				System.out.println("UniFi: " +resultado_auditorio+" | Status: "+stringunifiAuditorio_status+" | Experience: "+stringunifiAuditorio_experience);
+				System.out.println("Unifi: " +resultado_basese+ " |Status: "+stringunifiBasese_status+" | Experience: "+stringunifiBasese_experience);
 			
 			waitFor(5000);
+			}
+				
 		}
 		
 	}
@@ -122,7 +125,4 @@ public class Core {
 		botaoLogin.click();
 		
 	}
-
 }
-
-//teste
